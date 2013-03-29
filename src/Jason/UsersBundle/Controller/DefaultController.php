95f73fd93bdd6f328ae9286be8fe6346e0e4fcfe
Jason <jason.burns@126interactive.com>
2013-03-28 20:53:01 -0700

    Routing for bundle in yml format, added basic actions in DefaultController

--- src/Jason/UsersBundle/Controller/DefaultController.php
+++ src/Jason/UsersBundle/Controller/DefaultController.php
@@ -6,8 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('JasonUsersBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('JasonUsersBundle:Default:index.html.twig');
     }
+	
+	public function getallAction()
+	{
+		return $this->render('JasonUsersBundle:Default:getall.html.twig');
+	}
+	
+	public function getbynameAction()
+	{
+		return $this->render('JasonUsersBundle:Default:getbyname.html.twig');
+	}
+	
+	public function getuserdetail($id)
+	{
+		return $this->render('JasonUsersBundle:Default:getuserdetail.html.twig');
+	}
+	
+	public function getbylocale()
+	{
+		return $this->render('JasonUsersBundle:Default:getbylocale.html.twig');
+	}
 }
